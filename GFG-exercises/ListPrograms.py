5086a184e014541942b0030680c1a8584608954a
Sander <s.vandersomme@outlook.com>
2021-03-23 22:05:05 +0100

    Updated ListPrograms
    Added solutions to exercises even numbers up until remove empty tuples

--- GFG-exercises/ListPrograms.py
+++ GFG-exercises/ListPrograms.py
@@ -1,5 +1,6 @@
 from functools import reduce
 
+
 def swap_first_last_element(items):
     new_list = items
     new_list.append(items[0])
@@ -21,7 +22,7 @@ def is_present(items, element):
     return element in items
 
 
-def clear_list(items):
+def clear_list():
     return []
 
 
@@ -43,14 +44,63 @@ def n_largest(items, n):
     return sorted(items)[-n:]
 
 
+def even_numbers(items):
+    n = len(items)
+    while n - 1 >= 0:
+        if items[n - 1] % 2 != 0:
+            del items[n - 1]
+        n = n - 1
+    return items
+
+
+def odd_numbers(items):
+    n = len(items)
+    while n - 1 >= 0:
+        if items[n - 1] % 2 == 0:
+            del items[n - 1]
+        n -= 1
+    return items
+
+
+def even_in_range(start, end):
+    return list(filter(lambda x: (x % 2 == 0), list(range(start, end))))
+
+
+def odd_in_range(start, end):
+    return [x for x in list(range(start, end)) if (x % 2 != 0)]
+
+
+def positive_numbers(items):
+    return [x for x in items if (x % 2 != 0)]
+
+
+def negative_numbers(items):
+    return list(filter(lambda x: (x >= 0), items))
+
+
+def count_element(items, element):
+    return items.count(element)
+
+
+def remove_empty_tuples(tuples):
+    return list(filter(None, tuples))
+
+
 if __name__ == '__main__':
     print(swap_first_last_element([6, 7, 8, 4, 3, 2, 5, 6, 7, 9, 0]))
     print(swap_elements([6, 7, 8, 4, 3, 2, 5, 6, 7, 9, 0], 4, 6))
     print(find_length([6, 7, 8, 4, 3, 2, 5, 6, 7, 9, 0]))
     print(is_present([6, 7, 8, 4, 3, 2, 5, 6, 7, 9, 0], 1))
-    print(clear_list([6, 7, 8, 4, 3, 2, 5, 6, 7, 9, 0]))
+    print(clear_list())
     print(reverse_list([6, 7, 8, 4, 3, 2, 5, 6, 7, 9, 0]))
-    print(multiply_elements([3,4,5]))
-    print(second_largest([3,4,5]))
+    print(multiply_elements([3, 4, 5]))
+    print(second_largest([3, 4, 5]))
     print(n_largest([6, 7, 8, 4, 3, 2, 5, 6, 7, 9, 0], 3))
+    print(even_numbers([6, 7, 8, 4, 3, 2, 5, 6, 7, 9, 0]))
+    print(odd_numbers([6, 7, 8, 4, 3, 2, 5, 6, 7, 9, 0]))
+    print(even_in_range(2, 8))
+    print(odd_in_range(2, 8))
+    print(count_element([2, 4, 5, 8, 8, 7, 9, 8], 8))
+    print(remove_empty_tuples([(), (), (2, 4), (2, 5), ()]))
+
 
